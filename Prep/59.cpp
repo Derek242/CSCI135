233aef6e0011ea8c1c44ba0bb123e1d895554f5c
Derek242 <derek.ni47@myhunter.cuny.edu>
2023-01-24 18:18:59 -0500

    update

--- Prep/59.cpp
+++ Prep/59.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 
 int main(){
-    int annual;
-    int monthly;
+    float annual;
+    float monthly;
     cout << "Input your annual budget: ";
     cin >> annual;
     cout << "Input your month expense: ";
     cin >> monthly;
-    int inflation;
-    inflation = monthly * 0.15 + monthly
+    float inflation;
+    inflation = monthly * 0.15 + monthly;
+    cout << "month	expense	remaining balance of budget\n";
     for (int i = 1; i <= 12; i++){
-
-
-    }
+        if (i<=6){
+            cout << i << "             " << monthly << "               " << annual-monthly * i << endl;
     
+        }
+        else if (i>6){
+            cout << i << "              " << inflation << "             " << annual-inflation * i << endl;
+            
+        }
+        if (i==12 && annual-inflation*i >= 0){
+            cout << "You're on track";
+        }
+        else if (i==12 && annual-inflation*i < 0){
+            cout << "You need a higher budget";
+        }
+            
+        
+    }
+    cout << endl;
 }
